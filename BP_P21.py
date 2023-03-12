b50b4ad72ceea6e114555fb36f2a002624d5065a
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-12 14:45:54 +0530

    Add files via upload

---
+++ BP_P21.py
@@ -0,0 +1,17 @@
+# Basic Python Program
+
+# Palindrome or not a number
+
+num = int(input("Enter a number: "))
+temp = num
+rev = 0
+while num > 0:
+    dig = num % 10
+    rev = rev * 10 + dig
+    num = num // 10
+
+if temp == rev:
+    print("Number is Palindrome.")
+
+else:
+    print("Number is not Palindrome.")
